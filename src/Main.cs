98026a95bce64b1dbbabebe8ee0fcc12c2204559
BungusMuncher8 <9652749@graniteschools.org>
2026-09-17 13:22:43 -0600

    Add Main class with welcome functionality
    Implement basic structure for Main class with welcome message and player name entry.

---
+++ src/Main.cs
@@ -0,0 +1,23 @@
+//Mock because I can't create real project on mac
+
+private class Main 
+
+{
+  static Player player; 
+
+
+static void EntryWelcome()
+{
+string name = Console.ReadLine();
+  player.SetName(name);
+}
+  
+  static void Main(string[] args)
+    {
+           // player1.SpecailMove("YIPPEE");
+        Console.WriteLine("Welcome Bug Game");
+        EntryWelcome();
+
+    }
+
+}
